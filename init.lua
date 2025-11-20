60907c3b5eaaafa8975062be17c3cde6d9953fec
romanpotapov05 <mars777work@gmail.com>
2025-11-20 01:11:18 +0100

    Initialize lazy.nvim with Telescope and Tree-sitter
    This configuration file sets up lazy.nvim for plugin management, including Telescope and Tree-sitter for enhanced functionality and syntax highlighting.

---
+++ init.lua
@@ -0,0 +1,71 @@
+-- Загружаем lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+          },
+        },
+      })
+
+      telescope.load_extension("fzf")
+      telescope.load_extension("file_browser")
+
+      local builtin = require("telescope.builtin")
+
+      vim.keymap.set("n", "<space>ff", builtin.find_files)
+      vim.keymap.set("n", "<space>fg", builtin.live_grep)
+
+      vim.keymap.set("n", "<space>ft", function()
+        telescope.extensions.file_browser.file_browser({
+          path = vim.fn.expand("%:p:h"),
+          respect_gitignore = false,
+          hidden = true,
+          grouped = true,
+        })
+      end)
+    end,
+  },
+
+  -- Tree-sitter: красивая подсветка Java
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "java", "lua", "bash", "json", "vim", "regex" },
+        highlight = { enable = true },
+      })
+    end
+  },
+})
+
+-- Нумерация
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Мягкая подсветка строки
+vim.opt.cursorline = true
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2c2c2c" })
